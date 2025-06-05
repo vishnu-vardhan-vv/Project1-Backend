4bee5df8ddbcd5e810cdbba4c48c175d6bc18eb4
vishnu vardhan <vishnu4369@gmail.com>
2025-06-05 09:47:40 +0530

    go

--- utils/sendVerification.js
+++ utils/sendVerification.js
@@ -12,7 +12,9 @@ module.exports = async function(to, token) {
     await transporter.sendMail({
         to,
         subject: 'Verify Your Email',
+        // html: (`<h1>Email Verification</h1>
+        //     <p>Click <a href="http://localhost:5000/api/auth/verify/${token}">here</a> to verify.</p>`)
         html: (`<h1>Email Verification</h1>
-            <p>Click <a href="http://localhost:5000/api/auth/verify/${token}">here</a> to verify.</p>`)
+            <p>Click <a href="https://project1-backend-95xl.onrender.com/api/auth/verify/${token}">here</a> to verify.</p>`)
     });
 };
